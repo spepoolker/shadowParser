e3a6cd0b038cb9d918a16e4b47a64fa36d6f84d7
Sylvain Pennec <spennec@poolker.fr>
2024-10-16 18:39:14 +0200

    chore: cleanup ModuleUtils

--- src/main/kotlin/ModuleUtils.kt
+++ src/main/kotlin/ModuleUtils.kt
@@ -5,22 +5,22 @@ import java.io.File
 
 object ModuleUtils {
 
+  fun generatePropertiesLists() {
+    val yamlFile = File("data/all-modules.yml")
+    val data: List<Map<String, Any>> = Yaml().load(yamlFile.reader())
+    val result = data.map { item ->
+      val name = (item["name"] as String).replace("-", "_")
+      item["properties"]?.let { properties ->
+        val keys = (properties as Map<*, *>).keys
+        return@map "val ${camelToSnake(name).uppercase()}_SERVICES = listOf(${keys.joinToString(", ") { "\"$it\"" }})"
+      }
+    }
 
+    result.forEach { println(it) }
+  }
 
 }
 
 fun main() {
-  val yamlFile = File("data/all-modules.yml")
-  val yaml = Yaml()
-  val data: List<Map<String, Any>> = yaml.load(yamlFile.reader())
-  val result = data.map { item ->
-    val name = (item["name"] as String).replace("-", "_")
-    item["properties"]?.let {
-      val properties = (it as Map<*, *>).keys
-      return@map "val ${camelToSnake(name).uppercase()}_SERVICES = listOf(${properties.joinToString(", ") { "\"$it\"" }})"
-    }
-  }
-
-  // Print the result
-  result.forEach { println(it) }
+  ModuleUtils.generatePropertiesLists()
 }
